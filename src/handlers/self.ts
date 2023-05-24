4c6872e8695c184f0ef15a4ce6051af23dd9137b
J3 <gitlab@nevs.me>
2023-05-24 22:46:52 +0200

    fix for null values in getPlayerFromStorage

--- src/handlers/self.ts
+++ src/handlers/self.ts
@@ -10,6 +10,8 @@ export function definePlayer(playerName: string | null, female: boolean | null) 
 }
 
 export function getPlayerFromLocalStorage(): ReturnType<typeof definePlayer> {
+    if(localStorage.getItem("player") === null) return
+    if(localStorage.getItem("isFemale") === null) return
     const playerName = localStorage.getItem("player");
     const isFemale = JSON.parse(localStorage.getItem("isFemale") as string);
     return definePlayer(playerName.toUpperCase(), isFemale);
