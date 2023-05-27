e08aba589b39947374d622ec4ac13c892bc0d27a
J3 <gitlab@nevs.me>
2023-05-27 16:35:34 +0200

    refactor isEntity functions

--- src/handlers/modifiermaps/specialentities.ts
+++ src/handlers/modifiermaps/specialentities.ts
@@ -1,53 +1,54 @@
 export function isGhost(name: string): boolean {
-    return ghosts[name.toUpperCase()] === "ghost";
-}
-
-export function isGnome(name: string): boolean {
-    return gnomes[name.toUpperCase()] === "gnome";
-}
-
-const ghosts: { [word: string]: string; } = {
-    "GHOST VILLAGER": "ghost",
-    "GHOST SHOPKEEPER": "ghost",
-    "GHOST INNKEEPER": "ghost",
-    "ALICE'S HUSBAND": "ghost",
-    "GHOST DISCIPLE": "ghost",
-    "SHADY GHOST" : "ghost",
-    "NECROVARUS": "ghost",
-    "GRAVINGAS": "ghost",
-    "GHOST": "ghost",
-    "FILLIMAN TARLOCK": "ghost",
-    "NATURE SPIRIT": "ghost",
-    "THRANTAX THE MIGHTY": "ghost",
-    "SPIRIT": "ghost",
-    "SUMMER BONDE": "ghost",
-    "NIRRIE": "ghost",
-    "TIRRIE": "ghost",
-    "HALLAK": "ghost",
-    "MERANEK THANATOS": "ghost",
-    "ERIK BONDE": "ghost",
-    "JALLEK LENKIN": "ghost",
-    "ORMOD": "ghost",
-    "LENIAN": "ghost",
-    "EMACIATED SPIRIT": "ghost",
-}
-
-const gnomes: { [word: string]: string; } = {
-    "ANITA": "gnome",
-    "KING NARNODE SHAREEN": "gnome",
-    "NARNODE SHAREEN": "gnome",
-    "GNOME CHILD": "gnome",
-    "GNOME GUARD": "gnome",
-    "GNOME BALLER": "gnome",
-    "GNOME BALL REFEREE": "gnome",
-    "GNOME COACH": "gnome",
-    "GNOME TRAINER": "gnome",
-    "GNOME SHOPKEEPER": "gnome",
-    "GNOME WAITER": "gnome",
-    "GNOME WAITRESS": "gnome",
-    "GNOME": "gnome",
-    "DAERO": "gnome",
-    "GLOPHREN": "gnome",
-    "GLOUGH": "gnome",
-    "WAYDAR": "gnome",
-}
+    return ghosts.has(name.toUpperCase());
+  }
+  
+  export function isGnome(name: string): boolean {
+    return gnomes.has(name.toUpperCase());
+  }
+  
+  const ghosts: Set<string> = new Set([
+    "GHOST VILLAGER",
+    "GHOST SHOPKEEPER",
+    "GHOST INNKEEPER",
+    "ALICE'S HUSBAND",
+    "GHOST DISCIPLE",
+    "SHADY GHOST",
+    "NECROVARUS",
+    "GRAVINGAS",
+    "GHOST",
+    "FILLIMAN TARLOCK",
+    "NATURE SPIRIT",
+    "THRANTAX THE MIGHTY",
+    "SPIRIT",
+    "SUMMER BONDE",
+    "NIRRIE",
+    "TIRRIE",
+    "HALLAK",
+    "MERANEK THANATOS",
+    "ERIK BONDE",
+    "JALLEK LENKIN",
+    "ORMOD",
+    "LENIAN",
+    "EMACIATED SPIRIT",
+  ]);
+  
+  const gnomes: Set<string> = new Set([
+    "ANITA",
+    "KING NARNODE SHAREEN",
+    "NARNODE SHAREEN",
+    "GNOME CHILD",
+    "GNOME GUARD",
+    "GNOME BALLER",
+    "GNOME BALL REFEREE",
+    "GNOME COACH",
+    "GNOME TRAINER",
+    "GNOME SHOPKEEPER",
+    "GNOME WAITER",
+    "GNOME WAITRESS",
+    "GNOME",
+    "DAERO",
+    "GLOPHREN",
+    "GLOUGH",
+    "WAYDAR",
+  ]);
+  
